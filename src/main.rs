e7d9f71f2fc4739d16ab285109984cb97873e8e3
lxbme <18108274905@163.com>
2026-04-17 08:53:33 +0800

    chore: format main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use std::fs;
 use colored::Colorize;
+use std::fs;
 
-mod lexer;
 mod analyzer;
 mod compiler;
+mod lexer;
 mod vm;
-use lexer::{lexer, Scanner};
 use analyzer::analyzer;
 use compiler::compiler;
+use lexer::{Scanner, lexer};
 use vm::run_opcode;
 
 fn read_file(file_name: &String) -> String {
-    let content = fs::read_to_string(file_name)
-	.expect(format!("Cannot read file: {}", file_name).as_str());
+    let content =
+        fs::read_to_string(file_name).expect(format!("Cannot read file: {}", file_name).as_str());
     content
 }
 
@@ -20,20 +20,23 @@ fn main() {
     let content: String = read_file(&("./nested_test.sl".to_string()));
     let scanner = Scanner::new(content);
     let tokens = lexer(scanner);
-    
+
     //println!("Origin tokens: {:?} \n", tokens);
     match analyzer(&tokens) {
-	Ok(_) => println!("{}", "[Info] Grammar check passed.".green()),
-	//Ok(_) => {}
-	Err(e) => eprintln!("[ERROR] {}", e.red())
+        Ok(_) => println!("{}", "[Info] Grammar check passed.".green()),
+        //Ok(_) => {}
+        Err(e) => eprintln!("[ERROR] {}", e.red()),
     };
     let opcodes = match compiler(tokens) {
-	//Ok(opcodes) => println!{"Opcode: {:?}", opcodes},
-	Ok(opcodes) => opcodes,
-	Err(e) => {eprintln!("[ERROR] {}", e.red()); return ;}
+        //Ok(opcodes) => println!{"Opcode: {:?}", opcodes},
+        Ok(opcodes) => opcodes,
+        Err(e) => {
+            eprintln!("[ERROR] {}", e.red());
+            return;
+        }
     };
     match run_opcode(opcodes) {
-	Ok(_) => println!("{}", "\n[Info] finished...".green()),
-	Err(e) => eprintln!("[ERROR] {}", e.red())
+        Ok(_) => println!("{}", "\n[Info] finished...".green()),
+        Err(e) => eprintln!("[ERROR] {}", e.red()),
     }
 }
